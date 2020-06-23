4b25eae085017e0e183bb9fd60b34a2e2d01d16b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-23 09:55:41 +0200

    i18n

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Book Rating';
+  // title = 'Book Rating';
+  title = $localize`:@@BookRating:Book Rating`;
 }
--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -1,5 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { loadTranslations } from '@angular/localize';
+import { getTranslations, ParsedTranslationBundle } from '@locl/core';
+
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,7 +11,20 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  // TODO: load translations BEFORE starting the application
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// VORHER
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+// TODO: find a way to switch languages (eg. browser language or token in session storage)
+const messages = '/assets/messages.de.json';
+
+// NEU:
+getTranslations(messages).then(
+  (data: ParsedTranslationBundle) => {
+    loadTranslations(data.translations); // <-- this happens before bootstrap!
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  }
+);
