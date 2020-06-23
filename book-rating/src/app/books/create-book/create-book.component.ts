7899a5cbe2c7402cfa9e80e03843e9cababed2f4
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-23 12:01:32 +0200

    forms: part 1

---
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'br-create-book',
+  templateUrl: './create-book.component.html',
+  styleUrls: ['./create-book.component.scss']
+})
+export class CreateBookComponent {
+
+  bookForm = new FormGroup({
+    isbn: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    title: new FormControl('', Validators.required),
+    description: new FormControl()
+    // rating --> decide for a 3rd party lib first!
+  });
+
+}
