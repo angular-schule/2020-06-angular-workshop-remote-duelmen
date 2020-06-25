e955b9ba1d83068ac1235109b86b54fdaae0fe62
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-25 13:01:40 +0200

    rxjs: async pipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -8,14 +8,11 @@ import { map, filter, scan, reduce } from 'rxjs/operators';
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  isbn: string;
+  isbn$ = this.route.paramMap.pipe(
+    map(paramMap => paramMap.get('isbn'))
+  );
 
   constructor(private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    this.route.paramMap
-      .subscribe(paramMap => this.isbn = paramMap.get('isbn'));
-  }
 }
