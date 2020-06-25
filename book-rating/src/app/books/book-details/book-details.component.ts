e11a2f5e9b6f6d62e97dee684fe07a381819051a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-25 11:44:19 +0200

    rxjs: hands on (pipes)

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of, timer, Subscription, Subject, Observable } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit, OnDestroy {
+export class BookDetailsComponent implements OnInit {
 
   isbn: string;
-  private destroy$ = new Subject();
-
 
   constructor(private route: ActivatedRoute) { }
 
@@ -33,24 +31,36 @@ export class BookDetailsComponent implements OnInit, OnDestroy {
     // 2. Baustein: Observable
     // const observable1 = of('😎', '😇', '🤓');
 
-    const observable2 = new Observable(subscriber => {
+    const observable2 = new Observable<number>(subscriber => {
       subscriber.next(1);
       setTimeout(() => subscriber.next(2), 1000);
-      setTimeout(() => subscriber.next(3), 2000);
+      const x = setTimeout(() => { subscriber.next(3); console.log('Brennt das Licht noch?'); }, 2000);
       subscriber.next(4);
+      subscriber.next(5);
+      subscriber.next(6);
+      subscriber.next(7);
 
-      setTimeout(() => subscriber.complete(), 3000);
+      const y = setTimeout(() => subscriber.complete(), 3000);
+
+      return () => {
+        console.log('Machen wir mal schnell das Licht aus!');
+        clearTimeout(x);
+        clearTimeout(y);
+      };
     });
 
     // 3. Baustein: Subscription
-    observable2.subscribe(observer);
+    const subscription = observable2.pipe(
+      map(x => x * 10),
+      // Hands On
+      // 1. Filter alle Murmeln heraus, die kleiner sind als 30 (neu: 30, 40, 50, 60... 70)
+      // 2. Gib einmal die Summe aller Zahlen aus
+      // 3. Optional: Gib genau so viele Emojis aus, wie die Zahl groß ist
 
+    ).subscribe(observer);
+
+    setTimeout(() => subscription.unsubscribe(), 4000);
 
     // --------------------------
   }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-  }
-
 }
