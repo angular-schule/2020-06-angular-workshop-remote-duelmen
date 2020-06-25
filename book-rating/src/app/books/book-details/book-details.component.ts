3dee68b36b26ca7c4c8f7e10f26564881456d40a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-25 13:58:19 +0200

    rxjs: using share to make on cold observable hot

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of, timer, Subscription, Subject, Observable } from 'rxjs';
-import { map, filter, scan, reduce, mergeMap, concatMap, switchMap } from 'rxjs/operators';
+import { map, filter, scan, reduce, mergeMap, concatMap, switchMap, share } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -13,7 +13,8 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingleBook(isbn))
+    switchMap(isbn => this.bs.getSingleBook(isbn)),
+    share()
   );
 
   constructor(private route: ActivatedRoute,
