992b1f24c20e22d0ce0c48895c9a31fcfde8b291
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-26 11:12:32 +0200

    ng g feature books/store/book --module books/books --api --defaults

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { BooksModule } from './books/books.module';
 // import { BookComponent } from './books/book/book.component';
 import localeDe from '@angular/common/locales/de';
 import { registerLocaleData } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -22,7 +26,10 @@ import { registerLocaleData } from '@angular/common';
     AppRoutingModule,
     BooksModule,
     // AppRoutingEndModule
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [{
     provide: LOCALE_ID,
